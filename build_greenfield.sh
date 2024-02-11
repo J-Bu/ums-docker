6afaa90a173b5e52d711cc95bb4b02411ce567fe
Jan Burgmeier <jan.burgmeier@gmx.de>
2024-02-11 10:32:40 +0100

    Add /dev/dri device to console docker for hardware acceleration support if available Fix patching greenfield

--- build_greenfield.sh
+++ build_greenfield.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+git submodule init
+git submodule update
 docker build -t 'builder/greenfield' - < Dockerfile_build_greenfield
 docker run --rm -v "${PWD}/greenfield:/opt/greenfield:rw" -v "${PWD}/patches:/patches" --user ${UID}:${GID} builder/greenfield
